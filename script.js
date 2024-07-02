99f886297a365738eb54309ce96e9ddd715969af
KasselFelix <w-kassel.f@outlook.fr>
2024-07-02 14:31:19 +0200

    add type Book button

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
-document.querySelector("#search-btn")
-.addEventListener('click',function (){
+const data=[
+    {
+        departure:'paris',
+        arrival:'lyon',
+        date:'10/06/2024',
+        price:10
+    },
+    {
+        departure:'lyon',
+        arrival:'paris',
+        date:'10/06/2024',
+        price:9
+    }
+]
+
+document.querySelector('#search-btn').addEventListener('click',function (){
+    console.log('click')
     const departure = document.querySelector('#depart').value;
     const arrival = document.querySelector('#arrival').value;
     const date=document.querySelector('#date').value;
-
-	fetch('http://localhost:3000/trips',{
-		body: JSON.stringify({ departure:departure,arrival:arrival,date:date.toString() }),
-    })
-    .then(response => response.json())
-    .then(data => {
+    console.log(departure);
+    console.log(arrival);
+    console.log(date)
+	// fetch('http://localhost:3000/trips',{
+	// 	body: JSON.stringify({ departure:departure,arrival:arrival,date:date.toString() }),
+    // })
+    // .then(response => response.json())
+    // .then(data => {
         for (let trip of data) {
-            document.querySelectorAll('.tickets-data').innerHTML+=`
+            document.querySelector('#tickets-found').innerHTML+=`
             <div class="travel-data">${trip.departure}>${trip.arrival}</div>
-            <div class="travel-time">${trip.Date}</div>
+            <div class="travel-time">${trip.date}</div>
             <div class="travel-price">${trip.price}</div>
             <button class="book-btn"></button>
         `
         }
-    })
-    callDelete()
+    // })
+    //callDelete()
 })
 
 function callDelete(){
